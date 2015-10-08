adb77d67c0834d3adfba26d2b6a5c13753213d5f
Brit Butler <redline6561@gmail.com>
2015-10-07 22:22:54 -0400

    Add pseudocode for app functions.

--- lib/too_done.rb
+++ lib/too_done.rb
@@ -16,28 +16,39 @@ module TooDone
     option :date, :aliases => :d,
       :desc => "A Due Date in YYYY-MM-DD format."
     def add(task)
+      # find or create the right todo list
+      # create a new item under that list, with optional date
     end
 
-    desc "edit", "Choose a task to edit from a todo list."
+    desc "edit", "Edit a task from a todo list."
     option :list, :aliases => :l, :default => "*default*",
       :desc => "The todo list whose tasks will be edited."
     def edit
+      # find the right todo list
+      # BAIL if it doesn't exist and have tasks
+      # display the tasks and prompt for which one to edit
+      # allow the user to change the title, due date
     end
 
     desc "done", "Mark a task as completed."
     option :list, :aliases => :l, :default => "*default*",
       :desc => "The todo list whose tasks will be completed."
     def done
+      # find the right todo list
+      # BAIL if it doesn't exist and have tasks
+      # display the tasks and prompt for which one(s?) to mark done
     end
 
-    desc "show", "Show the tasks on a todo list in reverse chronological order."
+    desc "show", "Show the tasks on a todo list in reverse order."
     option :list, :aliases => :l, :default => "*default*",
       :desc => "The todo list whose tasks will be shown."
     option :completed, :aliases => :c, :default => false, :type => :boolean,
       :desc => "Whether or not to show already completed tasks."
     option :sort, :aliases => :s, :enum => ['history', 'overdue'],
-      :desc => "Sorting by 'history' (chronological, showing completed) or 'overdue'. Limits results to those with a due date."
+      :desc => "Sorting by 'history' (chronological) or 'overdue'. Limits results to those with a due date."
     def show
+      # find or create the right todo list
+      # show the tasks ordered as requested, default to reverse order (recently entered first)
     end
 
     desc "delete [LIST OR USER]", "Delete a todo list or a user."
@@ -46,10 +57,17 @@ module TooDone
     option :user, :aliases => :u,
       :desc => "The user which will be deleted (including lists and items)."
     def delete
+      # BAIL if both list and user options are provided
+      # BAIL unless a list or user options is provided
+      # find the matching user or list
+      # BAIL if the user or list couldn't be found
+      # delete them (and any dependents)
     end
 
     desc "switch USER", "Switch session to manage USER's todo lists."
     def switch(username)
+      # find or create an account matching USER
+      # add an entry to the "sessions" table that logs in USER
     end
   end
 end
