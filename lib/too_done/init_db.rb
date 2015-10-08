2e7757b57e22e6d51d109b1a0f6f5de1c427dd9d
Brit Butler <redline6561@gmail.com>
2015-10-07 22:59:07 -0400

    Conditionalize SQL logging on environment variable.

--- lib/too_done/init_db.rb
+++ lib/too_done/init_db.rb
@@ -3,5 +3,5 @@ require "yaml"
 
 config_path = File.expand_path('../../../config/database.yml', __FILE__)
 db_config = YAML::load(File.open(config_path))
-ActiveRecord::Base.logger = Logger.new(STDERR) if LOGGING
+ActiveRecord::Base.logger = Logger.new(STDERR) if ENV["LOGGING"]
 ActiveRecord::Base.establish_connection(db_config)
